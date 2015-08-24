d233213b125e7821e5db591cd3fb41ac0c3a8221
Ian Connor <iconnor@projectlounge.com>
2015-08-24 22:17:29 +1000

    Initial Commit

---
+++ simple_iphone/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  simple_iphone
+//
+//  Created by Ian Connor on 8/24/15.
+//  Copyright (c) 2015 Ian Connor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ simple_iphone/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  simple_iphone
+//
+//  Created by Ian Connor on 8/24/15.
+//  Copyright (c) 2015 Ian Connor. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
